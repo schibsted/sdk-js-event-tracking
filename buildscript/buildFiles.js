a220a63a79c4f51d76cd7c5568e2786257d10fd8
magska <magnus.skaalsveen@netlight.com>
2015-06-12 13:57:07 +0200

    buildscript and dependencies

---
+++ buildscript/buildFiles.js
@@ -0,0 +1,95 @@
+'use strict';
+
+var fs = require('fs'),
+	concat = require('concat-files'),
+	mkdirp = require('mkdirp'),
+	configPath = './configs/';
+
+fs.exists(configPath, function(exists) {
+	if (exists) {
+		getConfigFiles(configPath);
+	} else {
+		console.log('path not found');
+	}
+});
+
+function getConfigFiles(configPath) {
+	tryToCreateOutFolder();
+	getAllConfigFiles(fs.readdirSync(configPath));
+}
+
+function getAllConfigFiles(fileNameArray) {
+	if (fileNameArray.length > 0) {
+		var fileName = configPath + fileNameArray.pop();
+		console.log('building: ' + fileName);
+		if (isValidFile(fileName)) {
+			doFileMerge(fileName)
+			return getAllConfigFiles(fileNameArray);
+		}
+	} else {
+		return;
+	}
+}
+
+function isValidFile(fileName) {
+
+	var data = fs.readFileSync(fileName, 'utf8');
+	if (data) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function doFileMerge(fileName) {
+	
+	var buildFileName = './out/' + getBuildFileName(fileName);
+	var manifest = '/* SPT */';
+	
+	fs.writeFileSync(buildFileName, manifest);
+	fs.appendFileSync(buildFileName, readFile('./dist/autoTracker.min.js'));
+	fs.appendFileSync(buildFileName, 'var pulse2config = ' + readFile(fileName)) + ';';
+	
+	/*console.log('concat: ' + fileName);
+	return concat([
+		'./dist/autoTracker.min.js',
+		fileName
+	], './out/' + getBuildFileName(fileName), function(err) {
+		if (err) {
+			console.log(err);
+			return err;
+		}
+	});*/
+}
+
+function tryToCreateOutFolder() {
+	mkdirp('./out', function(err) {
+		if (err) {
+			return false;
+		} else {
+			return true;
+		}
+	})
+}
+
+function getBuildFileName(fileName) {
+	return 'autoTracker' + getClientId(fileName) + '.min.js';
+}
+
+function getClientId(fileName) {
+	var client = readAndParseFile(fileName).client;
+	return capitalizeFirstLetter(client);
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function readAndParseFile(fileName) {
+	var data = readFile(fileName);
+	return JSON.parse(data);
+}
+
+function readFile(fileName) {
+	return fs.readFileSync(fileName, 'utf8');
+}
